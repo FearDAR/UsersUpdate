f0d44398d263c32f6afd702dcff8ea62ed26390d
Dmitriy <135708205+FearDAR@users.noreply.github.com>
2024-01-30 14:19:01 +0400

    Initial commit

---
+++ src/main/java/com/example/usersupdate/UsersUpdateApplication.java
@@ -0,0 +1,13 @@
+package com.example.usersupdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersUpdateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersUpdateApplication.class, args);
+    }
+
+}
