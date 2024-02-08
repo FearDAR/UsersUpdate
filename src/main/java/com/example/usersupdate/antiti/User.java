ce2b54bf319e3ddf5b2375b1c8ee616f3c9bac8e
Dmitriy <135708205+FearDAR@users.noreply.github.com>
2024-02-08 17:08:12 +0400

    Add user entity and repository.

---
+++ src/main/java/com/example/usersupdate/antiti/User.java
@@ -0,0 +1,44 @@
+package com.example.usersupdate.antiti;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "second_naeme")
+    private String lastName;
+    @Column(name = "middle_name")
+    private String middleName;
+
+    public User() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+}
---
+++ src/main/java/com/example/usersupdate/antiti/UserRepository.java
@@ -0,0 +1,6 @@
+package com.example.usersupdate.antiti;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
