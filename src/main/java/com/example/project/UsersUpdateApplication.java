9d4052e2759dc1c3bc8d96d16aeeded1a573a3c5
Dmitriy <135708205+FearDAR@users.noreply.github.com>
2024-02-09 18:40:17 +0400

    Rename root folder.

--- src/main/java/com/example/project/UsersUpdateApplication.java
+++ src/main/java/com/example/project/UsersUpdateApplication.java
@@ -1,4 +1,4 @@
-package com.example.project.service;
+package com.example.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
