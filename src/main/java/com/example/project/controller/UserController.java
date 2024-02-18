094b5ded4bc40d9de05e78fb4ffca9e45a2432de
Dmitriy <135708205+FearDAR@users.noreply.github.com>
2024-02-18 17:25:52 +0400

    Made controller class.

---
+++ src/main/java/com/example/project/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping
+    public String returnHelloWorld(){
+        return "hello world";
+    }
+}
+
