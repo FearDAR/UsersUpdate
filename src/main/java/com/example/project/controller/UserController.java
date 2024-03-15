e3e7f71684fdfcfafdd307cca5fa051c6184a0d5
Dmitriy <135708205+FearDAR@users.noreply.github.com>
2024-03-15 15:50:14 +0400

    add enters.

--- src/main/java/com/example/project/controller/UserController.java
+++ src/main/java/com/example/project/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+
     @GetMapping
     public String returnHelloWorld(){
         return "hello world";
--- src/main/java/com/example/project/service/user/UserService.java
+++ src/main/java/com/example/project/service/user/UserService.java
@@ -8,11 +8,11 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
     private final UserRepository userRepository;
+
     public void create(UserCreatingRequest userCreatingRequest){
-
         User user = new User();
-
         user.setFirstName(userCreatingRequest.getFirstName());
         user.setMiddleName(userCreatingRequest.getMiddleName());
         user.setLastName(userCreatingRequest.getLastName());
